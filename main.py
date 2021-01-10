8b14f1247cd4bbbb4f06c08d20b313331b68bf39
frazermills <77229450+frazermills@users.noreply.github.com>
2021-01-10 16:01:23 +0000

    Update main.py
    I added pygame rendering, as well as the rules to run Conway's game of life

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 # title: Conway's Game of Life
 # date: 10.1.21
 
-import pygame, sys
+import pygame, sys, time
 from random import randrange
 
 def createCanvas(width, height, bgColour):
+	global screen
 	screen = pygame.display.set_mode((width, height))
 	pygame.display.set_caption("Conway's game of life")
 	screen.fill(bgColour)
@@ -24,6 +25,22 @@ def make2DArray(cols, rows):
 		arr[i] = [None] * rows
 	return arr;
 
+def drawCell(x, y, colour):
+	pygame.draw.rect(screen, colour, (x, y, 10, 10))
+	pygame.display.flip()
+
+def countNeighbours(grid, x, y, cols, rows, neighbourSum):
+	sum = 0
+	for i in range(-1, 2):
+		for j in range(-1, 2):
+			col = (x + i + cols) % cols
+			row = (y + j + rows) % rows
+			neighbourSum += grid[col][row]
+
+	neighbourSum -= grid[x][y]
+	return neighbourSum
+
+
 def setup(width, height, bgColour, resolution):
 	createCanvas(width, height, bgColour)
 	cols = width // resolution
@@ -35,23 +52,39 @@ def setup(width, height, bgColour, resolution):
 			grid[i][j] = randrange(2)
 	return [grid, cols, rows]
 
-def draw(grid, cols, rows, resolution):
+def draw(grid, cols, rows, resolution, colour):
 	for i in range(cols):
 		for j in range(rows):
 			x = i * resolution
 			y = j * resolution
 
 			if grid[i][j] == 1:
-				print("alive")
+				drawCell(x, y, colour)
+
+	nextArr = make2DArray(cols, rows)
+
+	for i in range(cols):
+		for j in range(rows):
+			state = grid[i][j]
+
+			neighbourSum = 0
+			neighbours = countNeighbours(grid, i, j, cols, rows, neighbourSum)
+
+			if state == 0 and neighbours == 3:
+				nextArr[i][j] = 1
+			elif state == 1 and (neighbours < 2 or neighbours > 3):
+				nextArr[i][j] = 0
 			else:
-				print("dead")
+				nextArr[i][j] = state
+
+	grid = nextArr
 
 def main():
 	grid = []
 	cols = 0
 	rows = 0
 	resolution = 10
-	width, height = 600, 400
+	width, height = 500, 500
 	BLACK = (0, 0, 0)
 	WHITE = (255, 255, 255)
 
@@ -61,7 +94,10 @@ def main():
 	cols = tempList[1]
 	rows = tempList[2]
 
-	draw(grid, cols, rows, resolution)
+	draw(grid, cols, rows, resolution, WHITE)
+	time.sleep(0.5)
+	
 
-if __name__ == '__main__':
-	main()
+for i in range(10):
+	if __name__ == '__main__':
+		main()
